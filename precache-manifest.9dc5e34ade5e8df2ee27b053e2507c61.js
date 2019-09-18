af399b8131228a849f780a77dabda71c88382e05
Mohamad Al kel <alkel@starcrm.com.my>
2019-09-18 12:02:41 +0800

    Updates

---
+++ precache-manifest.9dc5e34ade5e8df2ee27b053e2507c61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ca25dc5505ccc68c69ad78bcbb9c71",
+    "url": "/MohamadAlkelStar/cnc/index.html"
+  },
+  {
+    "revision": "3d57fa5715d75c5ff6e9",
+    "url": "/MohamadAlkelStar/cnc/static/css/main.55d9d7cf.chunk.css"
+  },
+  {
+    "revision": "7f20edcff382ed343800",
+    "url": "/MohamadAlkelStar/cnc/static/js/2.8cada222.chunk.js"
+  },
+  {
+    "revision": "3d57fa5715d75c5ff6e9",
+    "url": "/MohamadAlkelStar/cnc/static/js/main.b1ab8564.chunk.js"
+  },
+  {
+    "revision": "5da2b5c33ebb688fd537",
+    "url": "/MohamadAlkelStar/cnc/static/js/runtime~main.e14f1169.js"
+  },
+  {
+    "revision": "699ae1d7eff5b1d9e20bfda9aeeaac88",
+    "url": "/MohamadAlkelStar/cnc/static/media/car.699ae1d7.jpg"
+  }
+]);
